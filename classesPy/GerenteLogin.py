f2d1a2ed0282236febe3c97ed1ed16457bb11ea2
mashiro11 <thiagoaas1990@gmail.com>
2017-06-20 20:20:43 -0300

    inicio de comunicacao com base de dados

--- classesPy/GerenteLogin.py
+++ classesPy/GerenteLogin.py
@@ -1,8 +1,16 @@
 import cv2	# aqui é bom dizer quais funções vai importar, para não deixar o processo muito lento por importar tudo
+from enum import Enum
+from os import system
+
+import GerenteBD as GBD
 
 #mais informações sobre python orientado a objeto: https://www.tutorialspoint.com/python/python_classes_objects.htm
 #referencia para documentacao Doxygen: https://stackoverflow.com/questions/4302870/doxygen-syntax-in-python
 
+class TELA_INICIO(Enum):
+	LOGIN = 1
+	CADASTRO = 2
+	SAIR = 3
 
 class GerenteLogin:
 	"""@brief Classe responsável por gerenciar acesso do usuário ao sistema
@@ -14,7 +22,7 @@ class GerenteLogin:
 	tentar fazer o login via teclado, com um nome de usuário e senha.
 	"""
 	__sessaoFinalizada = False
-	def __init__(self, entrada):
+	def __init__(self):
 		"""@brief Construtor de GerenteLogin
 
 		Método de construção de GerenteLogin. No construtor, são estabelecidas
@@ -78,3 +86,45 @@ class GerenteLogin:
 		@return <return>
 		"""
 		print("Resolve login via teclado")
+
+	def TelaInicial(self):
+		opcao = 0
+		while(opcao != 3):
+			system('cls')
+			print("Bem vindo ao Sistema LINF!")
+			print("1 - Login")
+			print("2 - Novo cadastro")
+			print("3 - Sair")
+			opcao = int(input("Opcao: "))
+			#verificar como fazer o switch com enum!
+			if opcao == 1:
+				system('cls')
+				self.TelaLogin()
+			elif opcao == 2:
+				system('cls')
+				self.TelaCadastro()
+				opcao = 0
+			elif opcao == 3:
+				print("Saindo...")
+			else:
+				print("Opcao invalida!")
+
+	def TelaLogin(self):
+		user = []
+		login = input("Login: ")
+		senha = input("Senha: ")
+		user.append(login)
+		user.append(senha)
+		GBD.GerenteBD().BuscaUsuario(user)
+		return user
+
+	def TelaCadastro(self):
+		user = []
+		nome = input("Nome: ")
+		login = input("Login: ")
+		senha = input("Senha: ")
+		user.append(nome)
+		user.append(login)
+		user.append(senha)
+		GBD.GerenteBD().InsereUsuario(user)
+		input("Usuario cadastrado com sucesso! Pressione enter")
--- classesPy/mainScript.py
+++ classesPy/mainScript.py
@@ -5,8 +5,11 @@ import GerenteLogin as GLog
 #o Idela é que a classe main seja responsável
 #apenas por iniciar a execução do sistema,
 #passando o controle para uma das classes que a compõe
-entrada = "apenasDeTeste"
-objGLog = GLog.GerenteLogin(entrada)
+objGLog = GLog.GerenteLogin()
+objGLog.TelaInicial()
+
+"""
 objGDB = GBD.GerenteBD()
 while(objGLog.SessaoFinalizada() != True):
 	if(input()[0] == 'f'): objGLog.SetFimSessao(True)
+"""
